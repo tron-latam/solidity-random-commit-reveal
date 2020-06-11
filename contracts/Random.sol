8399307ecc046ef878261513f34c0afdc1be0a1c
cryptouru <cryptouru@gmail.com>
2020-06-11 18:02:15 -0300

    Contract comments and ehnancements

--- contracts/Random.sol
+++ contracts/Random.sol
@@ -2,21 +2,19 @@ pragma solidity >=0.5 <=0.5.10;
 import "@openzeppelin/contracts/access/Roles.sol";
 
 contract Random {
-    using Roles for Roles.Role;
-    Roles.Role private owners;
-    Roles.Role private oracles;
-    uint private numOracles;
+    uint constant MAX_DIGITS = 64;
 
-    bytes32 private sealedSeed;
+    using Roles for Roles.Role;
+    Roles.Role private _owners;
+    Roles.Role private _oracles;
+    uint private _numOracles;
+
+    bytes32 private _sealedSeed;
     uint public blocksToSkip = 7;
     uint public storedBlockNumber;
     bool public seedSet = false;
 
     uint[] public randomNumbers;
-
-    uint public blockNumber;
-    bytes32 public blockHashNow;
-    bytes32 public blockHashPrevious;
 
     event AddOracleEvent(address oracleAddress);
     event RemoveOracleEvent(address oracleAddress);
@@ -24,43 +22,50 @@ contract Random {
     event NewRandomNumber(uint number);
 
     modifier onlyOwner() {
-        require(owners.has(msg.sender), "Not owner");
+        require(_owners.has(msg.sender), "Not owner");
         _;
     }
 
     modifier onlyOracle() {
-        require(oracles.has(msg.sender), "Not owner");
+        require(_oracles.has(msg.sender), "Not owner");
         _;
     }
 
     constructor () public {
-        owners.add(msg.sender);
-        oracles.add(msg.sender);
-        numOracles++;
+        _owners.add(msg.sender);
+        _oracles.add(msg.sender);
+        _numOracles++;
     }
 
-    function addOracle (address _oracle) public {
-        require(!oracles.has(_oracle), "Already an oracle!");
-        oracles.add(_oracle);
-        numOracles++;
-        emit AddOracleEvent(_oracle);
+    function addOracle (address oracle) public {
+        require(!_oracles.has(oracle), "Already an oracle!");
+        _oracles.add(oracle);
+        _numOracles++;
+        emit AddOracleEvent(oracle);
     }
 
-    function removeOracle (address _oracle) public onlyOwner {
-        require(oracles.has(_oracle), "Address not an oracle");
-        require (numOracles > 1, "Unable to remove last oracle.");
-        oracles.remove(_oracle);
-        numOracles--;
-        emit RemoveOracleEvent(_oracle);
+    function removeOracle (address oracle) public onlyOwner {
+        require(_oracles.has(oracle), "Address not an oracle");
+        require (_numOracles > 1, "Unable to remove last oracle.");
+        _oracles.remove(oracle);
+        _numOracles--;
+        emit RemoveOracleEvent(oracle);
     }
 
-    function setBlocksToSkip (uint _blocks) public onlyOwner {
-        blocksToSkip = _blocks;
+    function setBlocksToSkip (uint blocks) public onlyOwner {
+        blocksToSkip = blocks;
     }
 
-    function setSealedSeed (bytes32 _sealedSeed) public onlyOracle {
+    /**
+     * @dev Sets encrypted random seed
+     * @param sealedSeed keccak256 encrypted seed
+     */
+    function setSealedSeed (bytes32 sealedSeed) public onlyOracle {
+        if (storedBlockNumber + 255 < block.number) {
+            seedSet = false;
+        }
         require(!seedSet, "Seed already set");
-        sealedSeed = _sealedSeed;
+        _sealedSeed = sealedSeed;
         storedBlockNumber = block.number + blocksToSkip;
         seedSet = true;
     }
@@ -69,13 +74,17 @@ contract Random {
         seedSet = false;
     }
 
-    function publishNumber (bytes32 _seed) public onlyOracle {
+    /**
+     * @dev Creates random number with raw unencrypted seed and publishes it
+     * @param seed unencrypted sealedSeed
+     */
+    function publishNumber (bytes32 seed) public onlyOracle {
         require(seedSet, "Seed yet to be set");
-        require(storedBlockNumber <= block.number, 'Waiting for mined block');
-        require(hash(_seed) == sealedSeed, 'Wrong seed');
+        require(storedBlockNumber < block.number, 'Waiting for mined block');
+        require(hash(seed) == _sealedSeed, 'Wrong seed');
         bytes32 hashToTest = blockhash(storedBlockNumber);
-        bytes32 newHash = keccak256(abi.encodePacked(_seed, hashToTest));
-        uint newNumber = uint(newHash);
+        bytes32 newHash = keccak256(abi.encodePacked(seed, hashToTest));
+        uint newNumber = uint(newHash) % (10 ** MAX_DIGITS);
         randomNumbers.push(newNumber);
         seedSet = false;
         emit NewRandomNumber(newNumber);
@@ -89,18 +98,48 @@ contract Random {
         return  randomNumbers.length;
     }
 
+    /**
+     * @dev Returns random numbers as a digits array
+     */
+    function getDigits() public view returns(uint[MAX_DIGITS] memory digits){
+            uint number = randomNumbers[randomNumbers.length - 1];
+            uint i = 0;
+            while (number > 0 && i < MAX_DIGITS) {
+                uint digit = uint(number % 10);
+                number = number / 10;
+                digits[i] = digit;
+                i++;
+            }
+            return digits;
+        }
+
+    /**
+     * @dev Returns random numbers as two a digits array
+     */
+    function getDoubleDigits() public view returns(uint[MAX_DIGITS / 2] memory digits){
+        uint number = randomNumbers[randomNumbers.length - 1];
+        uint i = 0;
+        while (number > 0 && i < MAX_DIGITS / 2) {
+            uint digit = uint(number % 100);
+            number = number / 100;
+            digits[i] = digit;
+            i++;
+        }
+        return digits;
+    }
+
     function isOwner(address account) public view returns (bool) {
-        return owners.has(account);
+        return _owners.has(account);
     }
 
     function isOracle(address account) public view returns (bool) {
-        return oracles.has(account);
+        return _oracles.has(account);
     }
 
-    function hash(bytes32 _toHash)
+    function hash(bytes32 toHash)
         public pure returns (bytes32)
     {
-        return keccak256(abi.encodePacked(_toHash));
+        return keccak256(abi.encodePacked(toHash));
     }
 
 }
